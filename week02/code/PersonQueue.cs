aa05338c72c2d9f98dea19856703829cca0d41e0
Mirabel Nwanneka <memeruwa@gmail.com>
2026-01-13 22:36:13 +0100

    checkin

--- week02/code/PersonQueue.cs
+++ week02/code/PersonQueue.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Collections.Generic;
+
 /// <summary>
-/// A basic implementation of a Queue
+/// A basic implementation of a Queue (FIFO)
 /// </summary>
 public class PersonQueue
 {
@@ -8,28 +11,27 @@ public class PersonQueue
     public int Length => _queue.Count;
 
     /// <summary>
-    /// Add a person to the queue
+    /// Add a person to the back of the queue
     /// </summary>
-    /// <param name="person">The person to add</param>
     public void Enqueue(Person person)
     {
-        _queue.Insert(0, person);
+        _queue.Add(person); // FIFO
     }
 
+    /// <summary>
+    /// Remove and return the person at the front of the queue
+    /// </summary>
     public Person Dequeue()
     {
+        if (_queue.Count == 0)
+            throw new InvalidOperationException("Queue is empty.");
+
         var person = _queue[0];
         _queue.RemoveAt(0);
         return person;
     }
 
-    public bool IsEmpty()
-    {
-        return Length == 0;
-    }
+    public bool IsEmpty() => _queue.Count == 0;
 
-    public override string ToString()
-    {
-        return $"[{string.Join(", ", _queue)}]";
-    }
+    public override string ToString() => $"[{string.Join(", ", _queue)}]";
 }
--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,61 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class SimpleQueue {
-    public static void Run() {
-        // Test 1
+public class SimpleQueue
+{
+    private readonly List<int> _queue = new();
+
+    public static void Run()
+    {
         Console.WriteLine("Test 1");
         var queue = new SimpleQueue();
         queue.Enqueue(100);
-        var value = queue.Dequeue();
-        Console.WriteLine(value);
+        Console.WriteLine(queue.Dequeue());
 
         Console.WriteLine("------------");
 
-        // Test 2
         Console.WriteLine("Test 2");
         queue = new SimpleQueue();
         queue.Enqueue(200);
         queue.Enqueue(300);
         queue.Enqueue(400);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
-        value = queue.Dequeue();
-        Console.WriteLine(value);
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
+        Console.WriteLine(queue.Dequeue());
 
         Console.WriteLine("------------");
 
-        // Test 3
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
-        try {
+        try
+        {
             queue.Dequeue();
             Console.WriteLine("Oops ... This shouldn't have worked.");
         }
-        catch (IndexOutOfRangeException) {
+        catch (IndexOutOfRangeException)
+        {
             Console.WriteLine("I got the exception as expected.");
         }
     }
 
-    private readonly List<int> _queue = new();
+    public void Enqueue(int value) => _queue.Add(value);
 
-    /// <summary>
-    /// Enqueue the value provided into the queue
-    /// </summary>
-    public void Enqueue(int value) {
-        _queue.Add(value); // add to back
-    }
-
-    /// <summary>
-    /// Dequeue the next value and return it
-    /// </summary>
-    public int Dequeue() {
+    public int Dequeue()
+    {
         if (_queue.Count == 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[0];   // remove from front
+        var value = _queue[0];
         _queue.RemoveAt(0);
         return value;
     }
