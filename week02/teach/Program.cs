dee327777d93596de00fd863eca09bd23a49b63c
Mirabel Nwanneka <memeruwa@gmail.com>
2026-01-14 21:12:43 +0100

    kill

--- week02/teach/Program.cs
+++ week02/teach/Program.cs
@@ -1,3 +1,12 @@
-﻿Console.WriteLine("\n======================\nCustomer Service\n======================");
-CustomerService.Run();
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\n======================\nCustomer Service\n======================");
+        CustomerService.Run();
+    }    
 // CustomerServiceSolution.Run();
+
+}
