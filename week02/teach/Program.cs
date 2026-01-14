2cbc119ac955cab5fed511c2da2bc105309e0188
Mirabel Nwanneka <memeruwa@gmail.com>
2026-01-14 22:32:33 +0100

    try

--- week02/teach/Program.cs
+++ week02/teach/Program.cs
@@ -9,5 +9,8 @@ class Program
         Console.WriteLine(ComplexStackSolution.CheckBraces("(a == 3 or (b == 5 and c == 6))")); // True
         Console.WriteLine(ComplexStackSolution.CheckBraces("(students]i].Grade > 80 and students[i].Grade < 90")); // False
         Console.WriteLine(ComplexStackSolution.CheckBraces("(robot[id + 1].Execute(.Pass() || (!robot[id * (2 + i)].Alive && stormy) || (robot[id - 1].Alive && lavaFlowing))")); // False
-    }    
+    }
+
+    //CustomerServiceSolution.Run();
 }
+
